4f36f3fb143bca2ea499b56352223b95f5e62562
Lukasz Trzaska <lukasz.trzaska@cern.ch>
2012-03-14 17:54:38 +0100

    * added .spec file for python-apscheduler \n * updated .spec file for xrdtest framework

--- rpmbuild/create_targz.sh
+++ rpmbuild/create_targz.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+        echo "CORRECT usage is:"
+	echo "create_targz.sh SRC_PATH"
+	exit 0
+fi
+echo "STARTING COPYING FILES FROM $1"
+
 ARCHIVE_DEST=$PWD
-NAME=XrdTestFramework-0.0.1
+NAME=xrdtest-0.0.1
 
 DEST=${ARCHIVE_DEST}/${NAME}
-SRC=/home/ltrzaska/dev/git-repos/xrootd-tests
-CP_OPTS=-vr
+SRC=$1
+CP_OPTS=-r
+
+set -e
 
 mkdir -p ${DEST}
 for n in Slave Master Hypervisor; do
@@ -19,7 +28,7 @@ for nl in slave master hypervisor; do
 	cp $CP_OPTS ${SRC}/certs/${nl}key.pem ${DEST}/certs
 done
 
-cp $CP_OPTS ${SRC}/webpage ${DEST}
+cp -r $CP_OPTS ${SRC}/webpage ${DEST}
 
 mkdir -p ${DEST}/clusters
 mkdir -p ${DEST}/testSuits
@@ -35,7 +44,9 @@ cp $CP_OPTS ${SRC}/lib/ClusterManager.py ${DEST}/lib
 cp $CP_OPTS ${SRC}/lib/uuid.py ${DEST}/lib
 
 mkdir -p ${DEST}/rpmbuild
-cp $CP_OPTS ${SRC}/rpmbuild/* ${DEST}/rpmbuild
+cp $CP_OPTS ${SRC}/rpmbuild/*.sh ${DEST}/rpmbuild
+cp $CP_OPTS ${SRC}/rpmbuild/xrdtest*d ${DEST}/rpmbuild
+cp $CP_OPTS ${SRC}/rpmbuild/*.spec ${DEST}/rpmbuild
 
 tar -cvzf ${NAME}.tar.gz ${NAME}
 rm -rf ${DEST}

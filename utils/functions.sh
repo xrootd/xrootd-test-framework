1f5637571c5760642899d1a4cace2c46b56457ba
Lukasz Janyst <ljanyst@cern.ch>
2013-10-18 17:04:45 +0200

    Clean up stage helper functions

--- utils/functions.sh
+++ utils/functions.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Set up the output streams and core directories
+#-------------------------------------------------------------------------------
 exec 2>&1
-set -e
-set -o errtrace
-set -o pipefail
 
-function error_trap () {
-    echo "script terminated with with error code $1"
-}
-
-function coredump_config () {
-    # Make sure corefiles are enabled and gdb is installed
-    yum -y -q install gdb > /dev/null
+yum -y -q install gdb > /dev/null
     
-    if ! grep -q "kernel.core_pattern" /etc/sysctl.conf; then
-        echo "kernel.core_pattern=/tmp/cores/core.%h.%p.%t" >> /etc/sysctl.conf
-    fi
+if ! grep -q "kernel.core_pattern" /etc/sysctl.conf; then
+   echo "kernel.core_pattern=/tmp/cores/core.%h.%p.%t" >> /etc/sysctl.conf
+fi
 
-    mkdir -p /tmp/cores; chmod a+rwx /tmp/cores
-    ulimit -c unlimited
+mkdir -p /tmp/cores; chmod a+rwx /tmp/cores
+ulimit -c unlimited
+
+#-------------------------------------------------------------------------------
+# Logging
+#-------------------------------------------------------------------------------
+function log()
+{
+  echo `date +"[%F %T]"` "$@"
 }
 
-function log () {
-    echo `date +['%T']` $@
+#-------------------------------------------------------------------------------
+# Run a command display it if failed and exit the script
+#-------------------------------------------------------------------------------
+function run()
+{
+  if [ $# -lt 1 ]; then
+    echo "[!] Run function requires the command"
+    exit 1
+  fi
+
+  OUTPUT=`eval $@ 2>&1`
+  if [ $? -ne 0 ]; then
+    log "[$@]: $OUTPUT"
+    exit 1
+  fi
 }
-
-function stamp () {
-    $@ | perl -p -MPOSIX -e 'BEGIN {$!=1} $_ = strftime("[%T]", localtime) . "\t" . $_'
-}
-
-function assert_fail () {
-    if "$@"; then 
-        log "$* didn't fail."
-        exit 1
-    else
-        log "$* failed as expected."
-    fi
-}
-
-trap 'e=$?; error_trap $e; exit $e' ERR
-
-rm -f /var/lib/rpm/__db*
-rpm --rebuilddb
-
-coredump_config
